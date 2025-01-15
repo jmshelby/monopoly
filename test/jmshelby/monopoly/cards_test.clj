336a330d21522b73cf87734d3ca6bffc64683c9f
Jake Shelby <1891693+jmshelby@users.noreply.github.com>
2025-01-14 22:43:21 -0700

    Better test naming

--- test/jmshelby/monopoly/cards_test.clj
+++ test/jmshelby/monopoly/cards_test.clj
@@ -17,27 +17,29 @@
 ;;   - next util
 ;;   - next railroad
 
-(deftest draw-move-back
-  (let [;; Setup game
-        before (-> (core/init-game-state 4)
-                   ;; with current player landing on community chest
-                   (update-in [:current-turn :dice-rolls] conj [3 4])
-                   (assoc-in [:players 0 :cell-residency] 7)
-                   ;; A certain amount of money
-                   (assoc-in [:players 0 :cash] 1500)
-                   ;; and just a "move back" card available
-                   (assoc-in [:card-queue :chance]
-                             [{:text           "Go Back 3 Spaces",
-                               :deck           :chance,
-                               :card/effect    :move,
-                               :card.move/cell [:back 3]}]))]
-    (let [after (cards/apply-card-draw before)]
-      ;; Moved back 3 and looped around
+(deftest draw
+  (testing "a 'Move Back' type card"
+    (let [;; Setup game
+          before (-> (core/init-game-state 4)
+                     ;; with current player landing on community chest
+                     (update-in [:current-turn :dice-rolls] conj [3 4])
+                     (assoc-in [:players 0 :cell-residency] 7)
+                     ;; A certain amount of money
+                     (assoc-in [:players 0 :cash] 1500)
+                     ;; and just a "move back" card available
+                     (assoc-in [:card-queue :chance]
+                               [{:text           "Go Back 3 Spaces",
+                                 :deck           :chance,
+                                 :card/effect    :move,
+                                 :card.move/cell [:back 3]}]))
+          ;; Run function in test
+          after  (cards/apply-card-draw before)]
+      ;; Assert, moved back 3 and looped around
       (is (= 4 (get-in after [:players 0 :cell-residency]))
           "Player moved back 3")
-      ;; Has a certain amount of cash after
-      ;; buying landing on tax AND
-      ;; did _not_ collect allowance
+      ;; Assert, has a certain amount of cash
+      ;; after buying landing on tax AND did
+      ;; _not_ collect allowance
       (is (= 1300 (get-in after [:players 0 :cash]))
           "Paid $200, cash balance"))))
 
