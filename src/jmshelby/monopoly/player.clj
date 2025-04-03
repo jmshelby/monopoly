cde713db49c883fcdd63ef66bd1cec7e13c17f47
Jake Shelby <1891693+jmshelby@users.noreply.github.com>
2025-04-02 20:40:54 -0600

    WIP - stubbing out new cond...

--- src/jmshelby/monopoly/player.clj
+++ src/jmshelby/monopoly/player.clj
@@ -1,38 +1,68 @@
 (ns jmshelby.monopoly.player
   (:require [jmshelby.monopoly.util :as util]))
 
-;; TODO - In order to implement the aquisition/transfer logic,
-;;        we need to indicate the target entity of the cash
+(defn- net-worth
+  [game-state player]
+  ;; Just need to add cash to property value
+  (+ (:cash player)
+     (util/player-property-sell-worth
+       game-state (:id player))))
+
 (defn make-requisite-payment
   "Given a game-state, player, and amount of cash, perform a requisite payment if possible.
   Four things can happen:
   - player has enough cash
-     -> subtract cash and execute follow-up, returning the new game-state
   - player doesn't have enough cash, but does have net worth..
-     -> execute 'raise money' workflow, force player to raise enough money and when they are there...
-     -> subtract the needed amount
-     -> execute follow-up logic, returning the new game-state
   - [owes bank] player doesn't have enough net worth to cover the amount needed
-     -> execute 'bankruptcy' workflow, but *don't* execute follow-up logic
-     -> mark player as bankrupt
-     -> mark player cash at zero
-     -> put all houses back into inventory
-     -> put all props back into inventory
-  - [owes opponent] player doesn't have enough net worth to cover the amount needed
-     -> execute 'bankruptcy' workflow, but *don't* execute follow-up logic
-     -> mark player as bankrupt
-     -> transfer player cash to opponent
-     -> put all houses back into inventory
-     -> execute property transfer/acquistion workflow to opponent
-        (asking target player their mortgaged options and applying)
-  "
-  [game-state player-id amount follow-up]
+  - [owes opponent] player doesn't have enough net worth to cover the amount needed"
+  [game-state
+   debtor debtee
+   amount follow-up]
+  ;; TODO - Should we also perform the transfer to debtee player??
+  (let [player    (util/player-by-id game-state debtor)
+        pidx      (:player-index player)
+        net-worth (partial net-worth game-state)]
 
-  ;; TODO - WIP .... This should do the simple first goal
-  ;; first iteration: just do what it does now ...
-  (let [player         (util/player-by-id game-state player-id)
-        pidx           (:player-index player)
-        with-deduction (update-in game-state
-                                  [:players pidx :cash]
-                                  - amount)]
-    (follow-up with-deduction)))
+    (cond
+
+      ;; -> player has enough cash
+      (<= amount (:cash player))
+      ;; * subtract cash and execute follow-up, returning the new game-state
+      (-> game-state
+          (update-in [:players pidx :cash]
+                     - amount)
+          follow-up)
+
+      ;; -> player doesn't have enough cash, but does have net worth..
+      (<= amount (net-worth player))
+      (-> game-state
+          ;; * execute 'raise money' workflow, force player to raise enough money and when they are there...
+          ;; * subtract cash and execute follow-up, returning the new game-state
+          (update-in [:players pidx :cash]
+                     - amount)
+          follow-up)
+
+      ;; -> [owes bank] player doesn't have enough net worth to cover the amount needed
+      (= :bank debtee)
+      ;; * execute 'bankruptcy' workflow, but *don't* execute follow-up logic
+      ;; * mark player as bankrupt
+      ;; * mark player cash at zero
+      ;; * put all houses back into inventory
+      ;; * put all props back into inventory
+
+      ;; -> [owes opponent] player doesn't have enough net worth to cover the amount needed
+      (string? debtee) ;; TODO - check that the player exists
+      ;; * execute 'bankruptcy' workflow, but *don't* execute follow-up logic
+      ;; * mark player as bankrupt
+      ;; * transfer player cash to opponent
+      ;; * put all houses back into inventory
+      ;; * execute property transfer/acquistion workflow to opponent
+      ;;   (asking target player their mortgaged options and applying)
+
+      :else
+      nil ;; TODO - throw exception
+      )
+
+
+
+    ))
